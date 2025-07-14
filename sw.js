4d2bd019a47a8ce27972ee5777b5128eac468240
nauh1012 <65577841+nauh1012@users.noreply.github.com>
2025-07-14 19:12:48 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,37 @@
+const CACHE_NAME = "kitsune-cache-v2";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./kitsune.png"
+];
+
+// Instala y guarda archivos en caché
 self.addEventListener("install", event => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open("kitsune-cache-v1").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./kitsune.png"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
 
+// Limpia versiones anteriores
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Intercepta las peticiones y devuelve del caché si está disponible
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
